85e1328c254836dd4fa1c2152845d7b5cd9585fb
Carolina-OH <carito.92@gmail.com>
2020-09-09 23:06:17 -0300

    uso de card para desplegar información

--- public/actions/actions.js
+++ public/actions/actions.js
@@ -1,24 +1,43 @@
-// const { nodeName } = require("jquery")
-// const { default: axios } = require("axios")
+const BASE_URL="https://corsanywhere.herokuapp.com/https://jobs.github.com/positions.json?"
+var pais = $("#pais").find(":selected").text();
+var afinidad= $("#afinidad").find(":selected").text();
 
-// var cumplePromesa = true;
-// const myFirstPromise = new Promise((resolve,reject)=>{
-// if (cumplePromesa){
-//     resolve("La promesa fue cumplida")
-// }else{
-//     reject("La promesa fue rota")
-// }
-// });
+$(function () {
+  $("#buscar").click(function (e) {
+    e.preventDefault(); 
+    console.log(afinidad , pais)
+    getdataJobs(afinidad,pais)
+  });
+  
+});
 
-// console.log(myFirstPromise);
 
-const getJobs = async () => {
-  try {
-    return await axios.get('https://corsanywhere.herokuapp.com/https://jobs.github.com/positions.json?')
-  } catch (error) {
+
+function getdataJobs (type, location){
+  axios.get(`${BASE_URL}description=${type}&location=${location}`)
+  .then((resp)=> {
+  resp.data.forEach((job)=>{
+    desplegarInfo(job)
+  })
+  console.log(resp)
+  })
+  .catch ((error)=> {
     console.error(error)
-  }
+  })
 }
-getJobs()
 
 
+
+function desplegarInfo(job){
+  var despliege=
+    `
+     <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+        <div class="card-header">${job.location}</div>
+        <div class="card-body">
+          <h5 class="card-title">Descripción</h5>
+          <p class="card-text">${job.description}</p>
+        </div>
+     </div>
+      `
+  $('#jobsContainer').append(despliege)
+}
